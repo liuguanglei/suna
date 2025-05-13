4ac43ae7c19defa1cac4f45c780ece94632c4d48
liuguanglei <liuguanglei@ainnovation.com>
2025-05-13 17:02:42 +0800

    c

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,15 @@
+echo "-------begin build backend-------"
 cd backend
 sudo docker build -t suna-backend . -f Dockerfile
 cd ..
+echo "-------build backend finished-------"
+
+echo "-------begin build frontend-------"
 cd frontend
 sudo docker build -t suna-frontend . -f Dockerfile
 cd ..
-sudo docker compose up -d
+echo "-------build frontend finished-------"
+
+echo "-------begin run docker compose-------"
+sudo docker-compose up -d
+echo "-------run docker compose finished-------"
