7cebec9665cdaba5dc57df764fcaf83344f7f063
liuguanglei <liuguanglei@ainnovation.com>
2025-08-07 18:42:03 +0800

    fix bugs

--- rebuild.sh
+++ rebuild.sh
@@ -4,11 +4,11 @@ sudo docker build -t suna-backend . -f Dockerfile
 cd ..
 echo "-------build backend finished-------"
 
-echo "-------begin build worker-------"
-cd backend
-sudo docker build -t suna-worker . -f Dockerfile
-cd ..
-echo "-------build worker finished-------"
+#echo "-------begin build worker-------"
+#cd backend
+#sudo docker build -t suna-worker . -f Dockerfile
+#cd ..
+#echo "-------build worker finished-------"
 
 echo "-------begin build frontend-------"
 cd frontend
